fc0256487e5538a43a7c48d7b91968d21db9ba26
Diego Weinmann <diegoweinmann@gmail.com>
2021-07-18 18:53:59 -0300

    add mongoose and mongoose module

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+
+const MONGO_URI = `mongodb://localhost:27017/foodtracker`;
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

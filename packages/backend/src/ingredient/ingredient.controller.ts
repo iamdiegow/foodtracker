2e2984f5918a20b2f78cdb76cef2e0b8b066829e
miguelangelparra <miguelangel.parradavila@gmail.com>
2021-07-25 07:18:26 -0300

    (feat): simple CRUD

--- packages/backend/src/ingredient/ingredient.controller.ts
+++ packages/backend/src/ingredient/ingredient.controller.ts
@@ -1,16 +1,36 @@
-import { Post, Get, Body, BadRequestException } from '@nestjs/common';
+import { Post, Get, Body, BadRequestException, Param, Put, Delete } from '@nestjs/common';
 import { Controller } from '@nestjs/common';
-import { CreateIndredientDto } from './dto/create-ingredient.dto';
+import { CreateIngredientDto } from './dto/create-ingredient.dto';
 import { IngredientService } from './ingredient.service';
 
 @Controller('ingredients')
 export class IngredientController {
-  constructor(private ingredientService: IngredientService) {}
+  constructor(private ingredientService: IngredientService) { }
 
   @Post()
   async createIngredient(
-    @Body() createIngredientDto: CreateIndredientDto,
+    @Body() createIngredientDto: CreateIngredientDto,
   ): Promise<any> {
     return await this.ingredientService.createIngredient(createIngredientDto);
+  }
+
+  @Get()
+  async findAllIngredients(): Promise<any> {
+    return await this.ingredientService.findAllIngredients()
+  }
+
+  @Get(":id")
+  async findOneIngredient(@Param("id") id: string): Promise<any> {
+    return await this.ingredientService.findOneIngredient(id)
+  }
+
+  @Put(":id")
+  async updateIngredient(@Param("id") id : string, @Body() updatedIngredient: CreateIngredientDto){
+    return await this.ingredientService.findAndUpdateIngredient(id,updatedIngredient)
+  }
+
+  @Delete(":id")
+  async deleteIngredient(@Param("id") id: string){
+    return await this.ingredientService.deleteIngredient(id)
   }
 }
--- packages/backend/src/ingredient/ingredient.service.ts
+++ packages/backend/src/ingredient/ingredient.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { CreateIndredientDto } from './dto/create-ingredient.dto';
+import { CreateIngredientDto } from './dto/create-ingredient.dto';
 
 interface Ingredient {
   name: string;
@@ -13,7 +13,25 @@ export class IngredientService {
     @InjectModel('Ingredient') private ingredientModel: Model<Ingredient>,
   ) {}
 
-  async createIngredient(createIngredientDto: CreateIndredientDto) {
+  async createIngredient(createIngredientDto: CreateIngredientDto) {
     return await this.ingredientModel.create(createIngredientDto);
+  }
+
+  async findAllIngredients():Promise<Ingredient[]>{
+    return await this.ingredientModel.find();
+  }
+
+  async findOneIngredient(id: string):Promise<Ingredient>{
+    return await this.ingredientModel.findById(id);
+  }
+//TODO ¿Tipado de retorno?
+  async findAndUpdateIngredient(id:string,updatedIngredient:CreateIngredientDto):Promise<any>{
+    const query = {"_id":id}
+    return await this.ingredientModel.findOneAndUpdate(query,updatedIngredient)
+  }
+//TODO ¿Tipado de retorno?
+  async deleteIngredient(id:string):Promise<any>{
+    const query = {"_id":id}
+    return await this.ingredientModel.remove(query)
   }
 }
